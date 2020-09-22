77a4edfb1ffb6043650701e52b6e70507e220b77
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2020-09-22 21:08:33 +0200

    Updates

---
+++ precache-manifest.4f2c4da194c9d3f786940f91c49ac53e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2213ea1b6f4b8603d6f2c1f9f77a0c13",
+    "url": "/registration_form/index.html"
+  },
+  {
+    "revision": "3b6d79f6fc79ca7e6fc0",
+    "url": "/registration_form/static/css/main.839de032.chunk.css"
+  },
+  {
+    "revision": "3525663c2d3bc5f6be93",
+    "url": "/registration_form/static/js/2.6c48768c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/registration_form/static/js/2.6c48768c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b6d79f6fc79ca7e6fc0",
+    "url": "/registration_form/static/js/main.1f5ac0bd.chunk.js"
+  },
+  {
+    "revision": "66e815231d47ceaedcbb",
+    "url": "/registration_form/static/js/runtime-main.eab80050.js"
+  }
+]);
